66b1dc6f43da7af6a6195b3450fc513affcea41b
NyanSilence <nyansilence@gmail.com>
2019-07-16 02:16:16 +0700

    update

--- 300x600v2/js/EBLoader.js
+++
@@ -1,4 +0,0 @@
-function loadSizmekScript(relativeUrl) {
-    document.write("<script src='" + (document.location.protocol === "https:" ? "https://secure-" : "http://") + "code.jquery.com//" + relativeUrl + "'><\/script>");
-}
-loadSizmekScript("jquery-3.4.1.min.js");
